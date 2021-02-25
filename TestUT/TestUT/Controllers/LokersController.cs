b331c38772e32861c50d0bf72f7ba88ea77e7a06
Lutfiarya <79649174+Lutfiarya@users.noreply.github.com>
2021-02-25 22:38:01 +0700

    Web Api Loker

---
+++ TestUT/TestUT/Controllers/LokersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestUT.Models;
+
+namespace TestUT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LokersController : Controller
+    {
+        
+        private MasterContext _context = new MasterContext();
+
+        // GET: api/Lokers
+        [HttpGet]
+        public IEnumerable<Loker> GetLoker()
+        {
+            
+            return _context.Loker.ToList();
+        }
+
+        // GET: api/Lokers/5
+        [HttpGet("{NomerLoker}")]
+        public IActionResult GetLoker( int NomerLoker)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var loker =  _context.Loker.Find(NomerLoker);
+
+            if (loker == null)
+            {
+                return NotFound();
+            }
+
+            return Ok("Loker nomor "+NomerLoker+ " ada di lantai "+ loker.LantaiLoker);
+        }
+
+      
+    }
+}
---
+++ TestUT/TestUT/Models/Loker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestUT.Models
+{
+    [Table("Loker")]
+    public class Loker
+    {
+        [Key]
+        public int IdLoker { get; set; }
+        public int LantaiLoker { get; set; }
+        public int NomerLoker { get; set; }
+    }
+}
